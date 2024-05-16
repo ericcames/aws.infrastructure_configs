333b47e66be79344bd0d1f951bb988164699431b
Eric Ames <ericcames@msn.com>
2024-05-16 09:59:37 -0700

    .

--- playbooks/scripts/my_secret.py
+++ playbooks/scripts/my_secret.py
@@ -2,6 +2,6 @@
 
 import os
 
-dynatrace_secret_api_key = os.environ['DYNATRACE_API_KEY']
+dynatrace_secret_api_key = os.environ['dynatrace_api_key']
 
 print(f'My dynatrace api secret is *** {dynatrace_secret_api_key} ***')
